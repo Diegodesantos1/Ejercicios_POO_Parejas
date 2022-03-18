628ce5d0d88072816862a6744cde2d68418c41e2
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 12:37:13 +0100

    arreglado el main y 10 segundos de sleep para el logger

--- Clases/Logger.py
+++ Clases/Logger.py
@@ -13,6 +13,5 @@ class Logger:
                 fichero.write(f" \n {str(i)} {mensaje}.")
         fichero.write(f"\n--End log: {str(numero)} log(s)--")
         fichero.close()
-        time.sleep (15) #se borra tras 15 segundos
+        time.sleep (10) #se borra tras 15 segundos
         os.remove("Logger.txt")
-Logger.llamada(int(input("¿Cuántos mensajes quieres?\n")))
