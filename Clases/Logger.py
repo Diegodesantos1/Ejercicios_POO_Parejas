c1a211b706cbda111c0adeb4b22555e2a47ae006
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 12:22:38 +0100

    eliminado los imports, ya que al final no son necesarios, y creado el bucle de las llamadas

--- Clases/Logger.py
+++ Clases/Logger.py
@@ -1,8 +1,14 @@
-import os
-import time
-
 class Logger:
     def llamada(n):
+        mensaje=input("Qué mensaje quieres escribir")
         print("Se va a generar el fichero de texto en unos instantes...")
         fichero = open("Logger.txt", "a")
         fichero.write("--Start log--")
+        for  i in range(1, n+1):
+            if i == 1:
+                fichero.write(f"\n {mensaje}.")
+            else:
+                fichero.write(f" \n {(i)} {mensaje}.")
+        fichero.write(f"\n--End log: {(n)} log(s)--")
+        fichero.close()
+Logger.llamada(input("¿Cuántos mensajes quieres?"))
