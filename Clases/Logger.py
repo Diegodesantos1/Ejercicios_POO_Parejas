c0aa85dcf9279693a25a693831f3c1770ddf2cd7
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 12:26:00 +0100

    da error con el número de llamada y el bucle

--- Clases/Logger.py
+++ Clases/Logger.py
@@ -1,14 +1,14 @@
 class Logger:
-    def llamada(n):
-        mensaje=input("Qué mensaje quieres escribir")
+    def llamada(numero):
+        mensaje=input("Qué mensaje quieres escribir\n")
         print("Se va a generar el fichero de texto en unos instantes...")
         fichero = open("Logger.txt", "a")
         fichero.write("--Start log--")
-        for  i in range(1, n+1):
+        for  i in range(1, numero+1):
             if i == 1:
                 fichero.write(f"\n {mensaje}.")
             else:
                 fichero.write(f" \n {(i)} {mensaje}.")
-        fichero.write(f"\n--End log: {(n)} log(s)--")
+        fichero.write(f"\n--End log: {(numero)} log(s)--")
         fichero.close()
-Logger.llamada(input("¿Cuántos mensajes quieres?"))
+Logger.llamada(input("¿Cuántos mensajes quieres?\n"))
