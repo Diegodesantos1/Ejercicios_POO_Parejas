045c4660b19c46cc7372b25120fd7b54c38d1620
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 08:53:57 +0100

    crada la clase y reutilizado el código de otro ejercicio ya usado antes, aunque no funciona todavía la clase

--- Ejercicio1/Clases/Palindromo.py
+++ Ejercicio1/Clases/Palindromo.py
@@ -1,19 +1,20 @@
-print("¿Que palabra, frase o número quieres comprobar para ver si es un palíndromo?")
-dato=input()
+class Palindromo:
+    print("¿Que palabra, frase o número quieres comprobar para ver si es un palíndromo?")
+    dato=input()
 
-i,j = "áéíóúñÁÉÍÓÚÑ" , "aeiounAEIOUN"
-acento=str.maketrans(i,j)
-dato=dato.translate(acento)
-dato=dato.lower() #Se transforma todo en minúsculas
-dato=dato.replace(" ", "") #Se quitan los espacios para que sea más sencillo
-lista = list(dato)
-listafinal= list(reversed(dato))
-def comprobar_palindromo(dato):
-  if len(dato) < 1:
-    print(f"La palabra o número {dato} es un palíndromo") #Caso para una sola letra o número
-  else:
-    if dato[0] == dato [-1]:
-      comprobar_palindromo(dato[1:-1])
-    else:
-      print(f"La palabra o número {dato} no es un palíndromo")
-comprobar_palindromo(dato)
+    i,j = "áéíóúñÁÉÍÓÚÑ" , "aeiounAEIOUN"
+    acento=str.maketrans(i,j)
+    dato=dato.translate(acento)
+    dato=dato.lower() #Se transforma todo en minúsculas
+    dato=dato.replace(" ", "") #Se quitan los espacios para que sea más sencillo
+    lista = list(dato)
+    listafinal= list(reversed(dato))
+    def comprobar_palindromo(dato):
+        if len(dato) < 1:
+            print(f"La palabra o número {dato} es un palíndromo") #Caso para una sola letra o número
+        else:
+            if dato[0] == dato [-1]:
+                Palindromo.comprobar_palindromo(dato[1:-1])
+            else:
+                print(f"La palabra o número {dato} no es un palíndromo")
+    Palindromo.comprobar_palindromo(dato)
