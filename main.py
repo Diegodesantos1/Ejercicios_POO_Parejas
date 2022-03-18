7f4d1575c3faeedb417c45a6399308658a9541f9
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 11:24:11 +0100

    funciona regular

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Clases import Palindromo
+from Clases.Palindromo import *
 
 def eleccion_ejercicio():
     variable = int(input("Seleccione que ejercicio desea ejecutar(1-3): "))
     if variable == 1:
-        Palindromo.comprobar_palindromo(Palindromo.input("¿Qué palabra/número quieres comprobar?"))
+        Palindromo.comprobar_palindromo()
     elif variable == 2:
         from Clases import ejercicio2
     elif variable == 3:
