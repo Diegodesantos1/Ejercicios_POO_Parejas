1cc3ce38c5771bfa2f3e9b41247316fd253b2319
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 11:24:17 +0100

    .

--- Clases/Palindromo.py
+++ Clases/Palindromo.py
@@ -1,14 +1,16 @@
 class Palindromo:
     def __init__(self,dato):
         self.dato=dato
-    i,j = "áéíóúñÁÉÍÓÚÑ" , "aeiounAEIOUN"
-    acento=str.maketrans(i,j)
-    dato=dato.translate(acento)
-    dato=dato.lower() #Se transforma todo en minúsculas
-    dato=dato.replace(" ", "") #Se quitan los espacios para que sea más sencillo
-    lista = list(dato)
-    listafinal= list(reversed(dato))
-    def comprobar_palindromo(dato):
+    def comprobar_palindromo():
+        dato=input(print("¿Qué palabra/número quieres comprobar?"))
+        i,j = "áéíóúñÁÉÍÓÚÑ" , "aeiounAEIOUN"
+        acento=str.maketrans(i,j)
+        dato=dato.translate(acento)
+        dato=dato.lower() #Se transforma todo en minúsculas
+        dato=dato.replace(" ", "") #Se quitan los espacios para que sea más sencillo
+        print(dato)
+        lista = list(dato)
+        listafinal= list(reversed(dato))
         if len(dato) < 1:
             print(f"La palabra o número es un palíndromo") #Caso para una sola letra o número
         else:
@@ -16,3 +18,4 @@ class Palindromo:
                 Palindromo.comprobar_palindromo(dato[1:-1])
             else:
                 print(f"La palabra o número no es un palíndromo")
+Palindromo.comprobar_palindromo()
