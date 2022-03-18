192f2a21a9fa747561c514275c8f34001c10329d
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 12:15:20 +0100

    arreglado el none, era culpa de un print dentro del input

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Clases.Palindromo import *
 def eleccion_ejercicio():
     variable = int(input("Seleccione que ejercicio desea ejecutar(1-3): "))
     if variable == 1:
-        dato=input(print("¿Qué palabra/número quieres comprobar?"))
+        dato=input("¿Qué palabra/número quieres comprobar?\n")
         Palindromo.comprobar_palindromo(dato)
     elif variable == 2:
         from Clases import ejercicio2
