1e23de4d21d435db74f7bc9dc0896e03e46a1844
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 12:37:19 +0100

    .

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Clases import Palindromo
 from Clases.Palindromo import *
+from Clases import Logger
+from Clases.Logger import *
 
 def eleccion_ejercicio():
     variable = int(input("Seleccione que ejercicio desea ejecutar(1-3): "))
@@ -11,7 +13,7 @@ def eleccion_ejercicio():
     elif variable == 3:
         from Clases import ejercicio3
     elif variable == 4:
-        from Clases import ejercicio4
+        Logger.llamada(int(input("¿Cuántos mensajes quieres?\n")))
     else:
         eleccion_ejercicio()
 
