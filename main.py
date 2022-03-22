79562d53019ade642ac0f0380c72e89f18c71439
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 09:18:25 +0100

    actualizado el main

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from Clases import Palindromo
 from Clases.Palindromo import *
 from Clases import Logger
 from Clases.Logger import *
-
+from Clases import Puzzle
+from Clases.Puzzle import *
 def eleccion_ejercicio():
     variable = int(input("Seleccione que ejercicio desea ejecutar: \n -->1: Palíndromo(método de clase)\n -->2: Palíndromo(método de instancias) \n -->3: Puzle \n -->4: Logger \n"))
     if variable == 1:
@@ -11,7 +12,7 @@ def eleccion_ejercicio():
     elif variable == 2:
         from Clases import Instancia
     elif variable == 3:
-        from Clases import ejercicio3
+        from Clases import Puzzle
     elif variable == 4:
         numero=int(input("¿Cuántos mensajes quieres? \n"))
         Logger.llamada(numero)
